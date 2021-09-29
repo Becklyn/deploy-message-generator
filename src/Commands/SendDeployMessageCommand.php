f0fd2605dacb523b0aa64ca2f2f7a8eeebe899bb
Pascal Gläßer <pg@becklyn.com>
2021-09-29 16:44:21 +0200

    Split CLI parsing and execution logic

--- src/Commands/SendDeployMessageCommand.php
+++ src/Commands/SendDeployMessageCommand.php
@@ -2,24 +2,29 @@
 
 namespace Becklyn\DeployMessageGenerator\Commands;
 
-use Becklyn\DeployMessageGenerator\SystemIntegration\ChatSystems\SlackChatSystem;
-use Becklyn\DeployMessageGenerator\SystemIntegration\TicketSystems\JiraTicketSytem;
-use Becklyn\DeployMessageGenerator\SystemIntegration\TicketSystems\TicketInfo;
-use Becklyn\DeployMessageGenerator\SystemIntegration\VersionControlSystems\GitVersionControlSystem;
+use Becklyn\DeployMessageGenerator\Runner\SendDeployMessageRunner;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Notifier\Exception\TransportExceptionInterface;
-use Symfony\Component\Process\Process;
 
 class SendDeployMessageCommand extends Command
 {
     private const COMMIT_RANGE_ARG_NAME = "commit-range";
     private const DEPLOY_STATUS_ARG_NAME = "deployment-status";
+    public const SEND_MESSAGE_FLAG_NAME = "send-message";
+    public const NON_INTERACTIVE_FLAG_NAME = "no-interaction";
     protected static $defaultName = "deployment:send-message";
+
+    private array $context;
+
+    public function __construct (array $context, ?string $name = null)
+    {
+        $this->context = $context;
+        parent::__construct($name);
+    }
 
 
     /**
@@ -30,6 +35,8 @@ class SendDeployMessageCommand extends Command
         parent::configure();
         $this->addArgument(self::DEPLOY_STATUS_ARG_NAME, InputArgument::REQUIRED, "The deployment status to be set. e.g. staging.");
         $this->addArgument(self::COMMIT_RANGE_ARG_NAME, InputArgument::REQUIRED, "The commit range that was deployed.");
+        $this->addOption(self::SEND_MESSAGE_FLAG_NAME, "m", InputOption::VALUE_NONE, "Will skip confirmation and send the message using the configured ChatSystem.");
+        $this->addOption(self::NON_INTERACTIVE_FLAG_NAME, "i", InputOption::VALUE_NONE, "Will prevent any interactions via the cli and just use default values");
         $this->setHelp("This command searches for all tickets that were deployed and creates a deploy message for them.");
     }
 
@@ -42,141 +49,14 @@ class SendDeployMessageCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $commitRange = $input->getArgument(self::COMMIT_RANGE_ARG_NAME);
         $deploymentStatus = $input->getArgument(self::DEPLOY_STATUS_ARG_NAME);
+        $this->context[self::SEND_MESSAGE_FLAG_NAME] = $input->hasOption(self::SEND_MESSAGE_FLAG_NAME);
+        $this->context[self::NON_INTERACTIVE_FLAG_NAME] = !$input->hasOption(self::NON_INTERACTIVE_FLAG_NAME);
 
-        $ticketSystem = new JiraTicketSytem($io);
-        $vcs = new GitVersionControlSystem($io);
-        $chatSystem = new SlackChatSystem($io);
-
-        $project = $this->getProjectName($io);
-        $ticketRegex = $ticketSystem->getTicketIdRegex();
-        $ticketIds = $vcs->getTicketIdsFromCommitRange($commitRange, $ticketRegex);
-        $tickets = [];
-
-        foreach ($ticketIds as $id) {
-            $ticketSystem->changeDeploymentStatus($id, $deploymentStatus);
-
-            $ticketInfo = $ticketSystem->getTicketInfo($id);
-            $tickets[] = $ticketInfo;
-        }
-
-        $question = new ConfirmationQuestion("Should the deployment message be sent using {$chatSystem->getName()}?");
-        $shouldSendMessageViaChatSystem = $io->askQuestion($question);
-
-        if ($shouldSendMessageViaChatSystem)
-        {
-            try
-            {
-                $chatMessage = $chatSystem->getChatMessage($tickets, $deploymentStatus, $project);
-                $chatSystem->sendMessage($chatMessage);
-            }
-            catch (TransportExceptionInterface $e)
-            {
-                $io->error("Could not send deploy message due. Generating message for copy/paste...");
-                $this->generateMessageForCopyPaste($tickets, $project, $deploymentStatus, $io);
-            }
-        }
-        else
-        {
-            $this->generateMessageForCopyPaste($tickets, $project, $deploymentStatus, $io);
-        }
+        (new SendDeployMessageRunner($io, $this->context))->run($commitRange, $deploymentStatus);
 
         return self::SUCCESS;
     }
 
 
-    /**
-     * @param TicketInfo[] $ticketInfos
-     */
-    private function generateMessageForCopyPaste(array $ticketInfos, string $project, string $deploymentStatus, SymfonyStyle $io) : void
-    {
-        $mdMessage = "`{$project}` has been deployed to `{$deploymentStatus}`.\n";
 
-        foreach ($ticketInfos as $ticketInfo)
-        {
-            // Markdown: " - [JIRA-101](jira.com/browser/WALD-101) Some ticket"
-            $mdMessage .= " - [{$ticketInfo->getId()}]({$ticketInfo->getUrl()}) {$ticketInfo->getTitle()}\n";
-        }
-
-        try {
-            $io->info("Copying the following markdown message:");
-            $io->write($mdMessage);
-            $this->copyToClipboard($mdMessage);
-            $io->success("Copied markdown message to clipboard");
-        }
-        catch (\Exception $e)
-        {
-            $io->error("Could not copy deployment message to clipboard. Please copy the markdown message above yourself.");
-        }
-    }
-
-    private function copyToClipboard(string $message) : void
-    {
-
-        switch (\PHP_OS_FAMILY)
-        {
-            case "Windows":
-                $executable = "clip";
-                break;
-
-            case "Darwin":
-                $executable = "pbcopy";
-                break;
-
-            case "BSD":
-            case "Solaris":
-            case "Linux":
-                $executable = "xclip";
-                break;
-
-            default:
-                throw new \Exception();
-        }
-
-        $process = new Process(["echo", $message, "|", $executable]);
-        $exitcode = $process->run();
-
-        if (0 !== $exitcode || !empty($process->getErrorOutput()))
-        {
-            throw new \Exception();
-        }
-    }
-
-    private function getProjectName(SymfonyStyle $io) : string
-    {
-        $cwd = \getcwd();
-
-        if (false === $cwd)
-        {
-            $io->error("Cannot read the path of the current working directory.");
-            throw new \Exception();
-        }
-
-        $dirname = \dirname($cwd);
-        $composerFilePath = "{$cwd}/composer.json";
-
-        if (!\is_file($composerFilePath))
-        {
-            $io->error("Cannot locate composer.json. Please run this command in the directory where the composer.json is located");
-            throw new \Exception();
-        }
-
-        $composerJson = \file_get_contents($composerFilePath);
-        $composer = \json_decode($composerJson, true, 512, \JSON_THROW_ON_ERROR);
-
-        if (empty($composer["name"]))
-        {
-            $io->error("Cannot read name from composer.json");
-            throw new \Exception();
-        }
-
-        $projectName = \explode("/", $composer["name"])[1];
-
-        // if dirname is $projectName with a suffix in form of "/-[a-zA-Z0-9]+/"
-        if (\strlen($dirname) > \strlen($projectName) && \str_starts_with($dirname, "{$projectName}-"))
-        {
-            $projectName = $dirname;
-        }
-
-        return $projectName;
-    }
 }
---
+++ src/Runner/SendDeployMessageRunner.php
@@ -0,0 +1,145 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\DeployMessageGenerator\Runner;
+
+use Becklyn\DeployMessageGenerator\Commands\SendDeployMessageCommand;
+use Becklyn\DeployMessageGenerator\Config\DeployMessageGeneratorConfig;
+use Becklyn\DeployMessageGenerator\SystemIntegration\TicketSystems\TicketInfo;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Notifier\Exception\TransportExceptionInterface;
+use Symfony\Component\Process\Process;
+
+class SendDeployMessageRunner
+{
+    private array $context;
+
+    private SymfonyStyle $io;
+
+    public function __construct (SymfonyStyle $io, array $context)
+    {
+        $this->io = $io;
+        $this->context = $context;
+    }
+
+    /**
+     * Runs the deployment Message Generation Process
+     */
+    public function run (string $commitRange, string $deploymentStatus) : void
+    {
+        $config = new DeployMessageGeneratorConfig();
+
+        if (!$config->isValidDeploymentStatus($deploymentStatus))
+        {
+            throw new \InvalidArgumentException("\"{$deploymentStatus}\" is not a valid environment. Did you configure it in the \".deploy-message-generator.yaml\" file?");
+        }
+
+
+        $ticketSystem = $config->getTicketSystem($this->io, $this->context);
+        $vcs = $config->getVersionControlSystem($this->io, $this->context);
+        $chatSystem = $config->getChatSystem($this->io, $this->context);
+
+        $project = $config->getProjectName();
+        $ticketRegex = $ticketSystem->getTicketIdRegex();
+        $deploymentStatus = $config->getDeploymentEnvironmentFor($deploymentStatus);
+
+        $ticketIds = $vcs->getTicketIdsFromCommitRange($commitRange, $ticketRegex);
+        $tickets = [];
+
+        foreach ($ticketIds as $id)
+        {
+            $ticketSystem->changeDeploymentStatus($id, $deploymentStatus);
+            $ticketInfo = $ticketSystem->getTicketInfo($id);
+            $tickets[] = $ticketInfo;
+        }
+
+        $shouldSendMessageViaChatSystem = $this->context[SendDeployMessageCommand::SEND_MESSAGE_FLAG_NAME];
+
+        // Interactive mode and no explicit flag to send message => ask
+        if ($this->context[SendDeployMessageCommand::NON_INTERACTIVE_FLAG_NAME] && !$shouldSendMessageViaChatSystem)
+        {
+            $question = new ConfirmationQuestion("Should the deployment message be sent using {$chatSystem->getName()}?", false);
+            $shouldSendMessageViaChatSystem = $this->io->askQuestion($question);
+        }
+
+        if ($shouldSendMessageViaChatSystem)
+        {
+            try
+            {
+                $chatMessage = $chatSystem->getChatMessage($tickets, $deploymentStatus, $project);
+                $chatSystem->sendMessage($chatMessage);
+            }
+            catch (TransportExceptionInterface $e)
+            {
+                $this->io->error("Could not send deploy message due. Generating message for copy/paste...");
+                $this->generateMessageForCopyPaste($tickets, $project, $deploymentStatus);
+            }
+        }
+        else
+        {
+            $this->generateMessageForCopyPaste($tickets, $project, $deploymentStatus);
+        }
+    }
+
+
+    /**
+     * @param TicketInfo[] $ticketInfos
+     */
+    private function generateMessageForCopyPaste (
+        array $ticketInfos,
+        string $project,
+        string $deploymentStatus
+    ) : void
+    {
+        $mdMessage = "`{$project}` has been deployed to `{$deploymentStatus}`." . \PHP_EOL;
+
+        foreach ($ticketInfos as $ticketInfo)
+        {
+            // Markdown: " - [JIRA-101](jira.com/browser/WALD-101) Some ticket"
+            $mdMessage .= " - [{$ticketInfo->getId()}]({$ticketInfo->getUrl()}) {$ticketInfo->getTitle()}" . \PHP_EOL;
+        }
+
+        try
+        {
+            $this->io->info("Copying the following markdown message:");
+            $this->io->write($mdMessage);
+            $this->copyToClipboard($mdMessage);
+            $this->io->success("Copied markdown message to clipboard");
+        }
+        catch (\Exception $e)
+        {
+            $this->io->error("Could not copy deployment message to clipboard. Please copy the markdown message above yourself.");
+        }
+    }
+
+
+    private function copyToClipboard (string $message) : void
+    {
+        switch (\PHP_OS_FAMILY)
+        {
+            case "Windows":
+                $executable = "clip";
+                break;
+
+            case "Darwin":
+                $executable = "pbcopy";
+                break;
+
+            case "BSD":
+            case "Solaris":
+            case "Linux":
+                $executable = "xclip";
+                break;
+
+            default:
+                throw new \Exception();
+        }
+        $process = new Process(["echo", $message, "|", $executable]);
+        $process->run();
+
+        if (!$process->isSuccessful())
+        {
+            throw new \Exception();
+        }
+    }
+}
