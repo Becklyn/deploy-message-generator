c5d27260ba75355cb93445e99adcf31756dab0fe
Pascal Gläßer <pg@becklyn.com>
2021-09-29 16:35:57 +0200

    Create Configuration

--- README.md
+++ README.md
@@ -12,14 +12,16 @@ composer global require becklyn/deploy-message-generator
 
 Required Environment Variables
 ------------------------------
+These variables can also be defined by placing `.deploy-message-generator.env` in your `$Home` (or `%USERPROFILE%` for Windows) directory.
 
 | Variable Name            | Purpose |
 | ------------------------ | ------------------------------------------------------------------------------------------------------------------------ |
 | SLACK_ACCESS_TOKEN       | Token needed to send messages to Slack                                                                                   |
-| SLACK_DEPLOYMENT_CHANNEL | The channel id where deployment messages should be sent to                                                               |
-| SLACK_TEST_CHANNEL       | The channel id where the tests in this package may sent messages to                                                      |
 |                          |                                                                                                                          |
-| JIRA_DOMAIN              | The base domain where the Jira installation lives. e.g. `example.atlassian.net`                                          |
 | JIRA_USER                | The user used to lookup the ticket information                                                                           |
 | JIRA_ACCESS_TOKEN        | The access token for the `JIRA_USER`                                                                                     | 
-| JIRA_TEST_TICKET         | A test ticket with has the summary `TEST TICKET FOR deploy-message-generator` which is used by the Jira integration test |
+
+Configuration
+-------------
+
+The generator is configured using the `.deploy-message-generator.yaml` file. Check the `examples` dir to learn more.
---
+++ src/Config/DeployMessageGeneratorConfig.php
@@ -0,0 +1,148 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\DeployMessageGenerator\Config;
+
+use Becklyn\DeployMessageGenerator\Exception\FileNotFoundException;
+use Becklyn\DeployMessageGenerator\Exception\FormatException;
+use Becklyn\DeployMessageGenerator\SystemIntegration\ChatSystems\ChatSystem;
+use Becklyn\DeployMessageGenerator\SystemIntegration\ChatSystems\SlackChatSystem;
+use Becklyn\DeployMessageGenerator\SystemIntegration\TicketSystems\JiraTicketSytem;
+use Becklyn\DeployMessageGenerator\SystemIntegration\TicketSystems\TicketSystem;
+use Becklyn\DeployMessageGenerator\SystemIntegration\VersionControlSystems\GitVersionControlSystem;
+use Becklyn\DeployMessageGenerator\SystemIntegration\VersionControlSystems\VersionControlSystem;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use function Symfony\Component\String\u;
+use Symfony\Component\Yaml\Yaml;
+
+class DeployMessageGeneratorConfig
+{
+    private array $config;
+    private String $file;
+    /** @var Array<string, string> */
+    private static array $environments = [];
+
+    public function __construct (?string $configDir = null)
+    {
+        if (empty($configDir))
+        {
+            $configDir = \getcwd();
+        }
+
+        if (false === $configDir)
+        {
+            throw new \Exception("Cannot read the path of the current working directory.");
+        }
+
+        $configFile = "{$configDir}/.deploy-message-generator.yaml";
+
+        if (!\is_file($configFile))
+        {
+            throw new FileNotFoundException("Cannot locate .deploy-message-generator.yaml. Please run this command from the root of your project");
+        }
+
+        $this->file = $configFile;
+        $this->config = Yaml::parseFile($configFile, Yaml::PARSE_EXCEPTION_ON_INVALID_TYPE);
+    }
+
+
+    public function getProjectName () : string
+    {
+        if (!isset($this->config["name"]))
+        {
+            throw new FormatException("\"name\" is not defined in \"{$this->file}\"");
+        }
+        return $this->config["name"];
+    }
+
+
+    public function getTicketSystem (SymfonyStyle $io, array $context) : TicketSystem
+    {
+        $systemName = $this->config["ticket-system"] ?? "jira";
+
+        if (isset($this->config[$systemName]))
+        {
+            $context[$systemName] = $this->config[$systemName];
+        }
+
+        return new JiraTicketSytem($io, $context, $this);
+    }
+
+
+    public function getVersionControlSystem (SymfonyStyle $io, array $context) : VersionControlSystem
+    {
+        $systemName = $this->config["vcs"] ?? "git";
+
+        if (isset($this->config[$systemName]))
+        {
+            $context[$systemName] = $this->config[$systemName];
+        }
+
+        return new GitVersionControlSystem($io, $context, $this);
+    }
+
+
+    public function getChatSystem (SymfonyStyle $io, array $context) : ChatSystem
+    {
+        $systemName = $this->config["chat-system"] ?? "slack";
+
+        if (isset($this->config[$systemName]))
+        {
+            $context[$systemName] = $this->config[$systemName];
+        }
+
+        return new SlackChatSystem($io, $context, $this);
+    }
+
+
+    /**
+     * @internal
+     */
+    public function getConfigFor (string $systemName) : array
+    {
+        if (empty($this->config[$systemName]))
+        {
+            return [];
+        }
+
+        return $this->config[$systemName];
+    }
+
+
+    public function isValidDeploymentStatus (string $deploymentStatus) : bool
+    {
+        return null !== $this->getDeploymentEnvironmentFor($deploymentStatus);
+    }
+
+    public function getDeploymentEnvironmentFor (string $environmentOrAlias) : ?string
+    {
+        if (empty(self::$environments))
+        {
+            foreach ($this->config["server"] as $environment => $aliases)
+            {
+                $environment = u($environment)->camel()->title()->toString();
+                self::$environments[$environment] = $environment;
+
+                if (empty($aliases))
+                {
+                    continue;
+                }
+
+                foreach ($aliases as $alias)
+                {
+                    $alias = u($alias)->camel()->title()->toString();
+                    self::$environments[$alias] = $environment;
+                }
+
+            }
+        }
+
+        $environmentOrAlias = u($environmentOrAlias)->camel()->title()->toString();
+
+        if (isset(self::$environments[$environmentOrAlias]))
+        {
+            return self::$environments[$environmentOrAlias];
+        }
+
+        return null;
+    }
+}
