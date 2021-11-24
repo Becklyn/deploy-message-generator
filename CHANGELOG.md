3f959190bfb4abf9d182eaf41a88ea2430f382d1
Kai Eichinger <ke@becklyn.com>
2021-11-24 16:48:00 +0100

    Manually bootstrap app in order to support installing/executing it via `composer global`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+1.0.1
+=====
+
+*   (bug) Manually bootstrap app in order to support installing and running it via `composer global`.
+
+
 1.0.0
 =====
 
--- src/BecklynDeployMessageGenerator.php
+++ src/BecklynDeployMessageGenerator.php
@@ -3,13 +3,23 @@
 use Becklyn\DeployMessageGenerator\Commands\SendDeployMessageCommand;
 use Composer\InstalledVersions;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Runtime\GenericRuntime;
 
-require \dirname(__DIR__) . "/vendor/autoload_runtime.php";
-
-return function (array $context)
+// Regular project installation
+if (\is_file($autoloader = \dirname(__DIR__) . "/vendor/autoload.php"))
 {
+    require_once $autoloader;
+}
+// Composer global install
+elseif (\is_file($autoloader = \dirname(__DIR__, 3) . "/autoload.php"))
+{
+    require_once $autoloader;
+}
+
+$app = static function (array $context) : Application {
     $home = "Windows" === \PHP_OS_FAMILY ? $context["USERPROFILE"] : $context["HOME"];
-    (new \Symfony\Component\Dotenv\Dotenv())->load("{$home}/.deploy-message-generator.env");
+    (new Dotenv())->load("{$home}/.deploy-message-generator.env");
     $context += $_ENV;
 
     $name = "becklyn/deploy-message-generator";
@@ -19,3 +29,20 @@ return function (array $context)
 
     return $application;
 };
+
+$runtime = new GenericRuntime(($_SERVER['APP_RUNTIME_OPTIONS'] ?? $_ENV['APP_RUNTIME_OPTIONS'] ?? []) + [
+    "disable_dotenv" => true,
+    "project_dir" => \dirname(__DIR__, 1),
+]);
+
+[$app, $args] = $runtime
+    ->getResolver($app)
+    ->resolve();
+
+$app = $app(...$args);
+
+exit(
+    $runtime
+        ->getRunner($app)
+        ->run()
+);
